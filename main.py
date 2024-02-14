6244e71b2f348afb40795bf43d8a93c66a3100d6
Ttrussardi <rustamtirdea@gmail.com>
2024-02-14 17:40:50 +0200

    First commit

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-# This is a sample Python script.
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+#
+# print("Hello")
+# print("Hello")
+# print("Hello")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# class Student:
+#     print("HI")
+#     def __init__(self):
+#         self.height = 160
+#         print("I am alive")
+#
+# first_student = Student()
+# second = Student()
+# print(first_student.height)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-print("Hello")
-print("Hello")
-print("Hello")
+# class Student:
+#     def __init__(self, height = 160):
+#         self.height = height
+# nick = Student()
+# kate = Student(height=170)
+# Chiril = Student(height=2033)
+# print(nick.height)
+# print(kate.height)
+# print(Chiril. height)
